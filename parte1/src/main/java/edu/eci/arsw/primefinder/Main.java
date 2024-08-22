927ae5d928cc64fad13f39301f1de12ae7071238
unknown <hann.jang@labinfo.is.escuelaing.edu.co>
2024-08-22 09:46:25 -0500

    Parte I terminado

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,45 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+
+		//Realizado con un Thread
+		//PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		//pft.start();
+
+		final Object pauseLock = new Object();
+		PrimeFinderThread[] threads = new PrimeFinderThread[3];
+
+		for (int i=0; i<threads.length; i++) {
+			threads[i] = new PrimeFinderThread(i * 10000000, (i + 1) * 10000000 -1 ,pauseLock);
+			threads[i].start();
+		}
+
+		boolean isRunning =true;
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (isRunning){
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+			if (Thread.activeCount() == 2) {
+				isRunning = false;
+			}
+
+			System.out.println("Presione Enter para continuar: ");
+			scanner.nextLine();
+
+			synchronized (pauseLock) {
+				pauseLock.notifyAll();
+			}
+
+			}
+		}
 	}
-	
-}
+
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -1,30 +1,48 @@
 package edu.eci.arsw.primefinder;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.Currency;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class PrimeFinderThread extends Thread{
 
 	
-	int a,b;
+	int a, b;
+	private final Object pauselock;
 	
-	private List<Integer> primes=new LinkedList<Integer>();
+	private List<Integer> primeNumbers=new LinkedList<Integer>();
 	
-	public PrimeFinderThread(int a, int b) {
+	public PrimeFinderThread(int a, int b, Object pauselock) {
 		super();
 		this.a = a;
 		this.b = b;
+		this.pauselock = pauselock;
 	}
 
 	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
-				primes.add(i);
+		long startTime = System.currentTimeMillis();
+
+		for (int i=a;i<=b;i++){
+			if (System.currentTimeMillis() - startTime  >= 5000 ){
+				pauseExecution();
+				startTime = System.currentTimeMillis();
+			}else if (isPrime(i)) {
+				primeNumbers.add(i);
 				System.out.println(i);
 			}
 		}
-		
-		
+	}
+
+	private void pauseExecution() {
+		synchronized (pauselock){
+			try {
+				pauselock.wait();
+			} catch (InterruptedException e){
+				Thread.currentThread().interrupt();
+			}
+		}
 	}
 	
 	boolean isPrime(int n) {
@@ -36,8 +54,8 @@ public class PrimeFinderThread extends Thread{
 	    return true;
 	}
 
-	public List<Integer> getPrimes() {
-		return primes;
+	public List<Integer> getPrimeNumbers() {
+		return primeNumbers;
 	}
 	
 	
